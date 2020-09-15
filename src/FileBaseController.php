9ce607958a86dd2ada9e1672f19e1f49a41c1dba
Jared Rosenberg <jrosenberg@rootinc.com>
2020-09-15 16:10:47 -0400

    Tests updates and method docblocks

--- src/FileBaseController.php
+++ src/FileBaseController.php
@@ -9,162 +9,162 @@ use Illuminate\Http\Request;
 
 class FileBaseController extends Controller
 {
-    public function index(Request $request)
-    {
-        $search = $request->input('search') ? $request->input('search') : "";
+	public function index(Request $request)
+	{
+		$search = $request->input('search') ? $request->input('search') : "";
 
-        $files = File::where(function($q) use ($search)  {
-            $q->where('file_name', 'ILIKE', "%$search%")
-            ->orWhere('title', 'ILIKE', "%$search%");
-        })
-        ->orderBy('file_name')
-        ->paginate();
+		$files = File::where(function($q) use ($search)  {
+			$q->where('file_name', 'ILIKE', "%$search%")
+				->orWhere('title', 'ILIKE', "%$search%");
+		})
+			->orderBy('file_name')
+			->paginate();
 
-        return response()->json([
-            'status' => "success",
-            'payload' => [
-                'files' => $files,
-            ]
-        ]);
-    }
+		return response()->json([
+			'status' => "success",
+			'payload' => [
+				'files' => $files,
+			]
+		]);
+	}
 
-    /**
-     * Creates and uploads a file
-     *
-     * @param Illuminate\Http\Request $request
-     * @return Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $file_name = $request->input('file_name');
-        $file_type = $request->input('file_type');
-        $file_data = $request->input('file_data');
+	/**
+	 * Creates and uploads a file
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @return \Illuminate\Http\JsonResponse
+	 */
+	public function store(Request $request)
+	{
+		$file_name = $request->input('file_name');
+		$file_type = $request->input('file_type');
+		$file_data = $request->input('file_data');
 
-        $relative_directory = $request->input('directory') ?? null;
-        $public = $request->input('public') ?? false;
+		$relative_directory = $request->input('directory') ?? null;
+		$public = $request->input('public') ?? false;
 
-        //this is set up with two distinct routes so that the FileUploader component can still call the `store / post` method on the file object
-        //in the cloud route, we are doing a direct upload to s3
-        //in the other route, we are doing a an upload to the server, then to s3
-        if (!$file_data)
-        {
-            $data = File::s3CreateUpload(new File, $file_name, $file_type, $relative_directory, $public);
-            $file = $data['file'];
-            $upload_url = $data['upload_url'];
+		//this is set up with two distinct routes so that the FileUploader component can still call the `store / post` method on the file object
+		//in the cloud route, we are doing a direct upload to s3
+		//in the other route, we are doing a an upload to the server, then to s3
+		if (!$file_data)
+		{
+			$data = File::s3CreateUpload(new File, $file_name, $file_type, $relative_directory, $public);
+			$file = $data['file'];
+			$upload_url = $data['upload_url'];
 
-            $file->refresh();
+			$file->refresh();
 
-            return response()->json([
-                'status' => "success",
-                'payload' => [
-                    'file' => $file,
-                    'upload_url' => $upload_url,
-                ]
-            ]);
-        }
-        else
-        {
-            $file = File::uploadAndCreateFileFromDataURI($file_name, $file_type, $file_data, $relative_directory, $public);
-            $file->refresh();
+			return response()->json([
+				'status' => "success",
+				'payload' => [
+					'file' => $file,
+					'upload_url' => $upload_url,
+				]
+			]);
+		}
+		else
+		{
+			$file = File::uploadAndCreateFileFromDataURI($file_name, $file_type, $file_data, $relative_directory, $public);
+			$file->refresh();
 
-            return response()->json([
-                'status' => "success",
-                'payload' => [
-                    'file' => $file
-                ]
-            ]);
-        }
-    }
+			return response()->json([
+				'status' => "success",
+				'payload' => [
+					'file' => $file
+				]
+			]);
+		}
+	}
 
-    /**
-     * Updates and uploads replacement file
-     *
-     * @param Illuminate\Http\Request $request
-     * @return Illuminate\Http\Response
-     */
-    public function update(Request $request, File $file)
-    {
-        //this is set up with two distinct routes so that the FileUploader component can still call the `update / put` method on the file object
-        //in the title route, we only need to update the title of the file
-        //in the other route, we are doing a replacement file
-        if ($request->input('title'))
-        {
-            $file->title = $request->input('title');
-            $file->save();
+	/**
+	 * Updates and uploads replacement file
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @return \Illuminate\Http\JsonResponse
+	 */
+	public function update(Request $request, File $file)
+	{
+		//this is set up with two distinct routes so that the FileUploader component can still call the `update / put` method on the file object
+		//in the title route, we only need to update the title of the file
+		//in the other route, we are doing a replacement file
+		if ($request->input('title'))
+		{
+			$file->title = $request->input('title');
+			$file->save();
 
-            return response()->json([
-                'status' => "success",
-                'payload' => [
-                    'file' => $file
-                ]
-            ]);
-        }
-        else
-        {
-            File::deleteUpload($file->location);
+			return response()->json([
+				'status' => "success",
+				'payload' => [
+					'file' => $file
+				]
+			]);
+		}
+		else
+		{
+			File::deleteUpload($file->location);
 
-            $file_name = $request->input('file_name');
-            $file_type = $request->input('file_type');
-            $file_data = $request->input('file_data');
+			$file_name = $request->input('file_name');
+			$file_type = $request->input('file_type');
+			$file_data = $request->input('file_data');
 
-            $relative_directory = $request->input('directory') ?? null;
-            $public = $request->input('public') ?? false;
+			$relative_directory = $request->input('directory') ?? null;
+			$public = $request->input('public') ?? false;
 
-            //this is set up with two distinct routes so that the FileUploader component can still call the `update / put` method on the file object
-            //in the cloud route, we are doing a direct upload to s3
-            //in the other route, we are doing a an upload to the server, then to s3
-            if (!$file_data)
-            {
-                $data = File::s3CreateUpload($file, $file_name, $file_type, $relative_directory, $public);
-                $file = $data['file'];
-                $upload_url = $data['upload_url'];
+			//this is set up with two distinct routes so that the FileUploader component can still call the `update / put` method on the file object
+			//in the cloud route, we are doing a direct upload to s3
+			//in the other route, we are doing a an upload to the server, then to s3
+			if (!$file_data)
+			{
+				$data = File::s3CreateUpload($file, $file_name, $file_type, $relative_directory, $public);
+				$file = $data['file'];
+				$upload_url = $data['upload_url'];
 
-                $file->refresh();
+				$file->refresh();
 
-                return response()->json([
-                    'status' => "success",
-                    'payload' => [
-                        'file' => $file,
-                        'upload_url' => $upload_url,
-                    ]
-                ]);
-            }
-            else
-            {
-                $uploadedFile = File::makeUploadFileFromDataURI($file_name, $file_type, $file_data, $relative_directory, $public);
-                $upload_location = File::upload($uploadedFile, null, true);
+				return response()->json([
+					'status' => "success",
+					'payload' => [
+						'file' => $file,
+						'upload_url' => $upload_url,
+					]
+				]);
+			}
+			else
+			{
+				$uploadedFile = File::makeUploadFileFromDataURI($file_name, $file_type, $file_data);
+				$upload_location = File::upload($uploadedFile, $relative_directory, $public);
 
-                $file->file_name = $file_name;
-                $file->file_type = $uploadedFile->getClientMimeType();
-                $file->location = $upload_location;
+				$file->file_name = $file_name;
+				$file->file_type = $uploadedFile->getClientMimeType();
+				$file->location = $upload_location;
 
-                $file->save();
+				$file->save();
 
-                return response()->json([
-                    'status' => "success",
-                    'payload' => [
-                        'file' => $file
-                    ]
-                ]);
-            }
-        }
-    }
+				return response()->json([
+					'status' => "success",
+					'payload' => [
+						'file' => $file
+					]
+				]);
+			}
+		}
+	}
 
-    /**
-     * Deletes and deletes uploaded file
-     *
-     * @param Illuminate\Http\Request $request
-     * @return Illuminate\Http\Response
-     */
-    public function destroy(Request $request, File $file)
-    {
-        File::deleteUpload($file->location);
+	/**
+	 * Deletes and deletes uploaded file
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @return \Illuminate\Http\JsonResponse
+	 */
+	public function destroy(Request $request, File $file)
+	{
+		File::deleteUpload($file->location);
 
-        $file->delete();
+		$file->delete();
 
-        return response()->json([
-            'status' => "success",
-            'payload' => []
-        ]);
-    }
+		return response()->json([
+			'status' => "success",
+			'payload' => []
+		]);
+	}
 }
--- src/FileBaseControllerTest.php
+++ src/FileBaseControllerTest.php
@@ -12,123 +12,149 @@ use App\User;
 
 class FileBaseControllerTest extends TestCase
 {
-    use RefreshDatabase;
+	use RefreshDatabase;
 
-    protected function getUserForIndex()
-    {
-        return factory(User::class)->create();
-    }
+	protected function getUserForIndex()
+	{
+		return factory(User::class)->create();
+	}
 
-    protected function getUserForIndexError()
-    {
-        return factory(User::class)->create();
-    }
+	protected function getUserForIndexError()
+	{
+		return factory(User::class)->create();
+	}
 
-    protected function getUserForCreate()
-    {
-        return factory(User::class)->create();
-    }
+	protected function getUserForCreate()
+	{
+		return factory(User::class)->create();
+	}
 
-    protected function getUserForUpdate()
-    {
-        return factory(User::class)->create();
-    }
+	protected function getUserForUpdate()
+	{
+		return factory(User::class)->create();
+	}
 
-    protected function getUserForDelete()
-    {
-        return factory(User::class)->create();
-    }
+	protected function getUserForDelete()
+	{
+		return factory(User::class)->create();
+	}
 
-    /** @test */
-    public function it_checks_if_index_work()
-    {
-        $files = factory(File::class, 4)->create();
+	/** @test */
+	public function it_checks_if_index_work()
+	{
+		$files = factory(File::class, 4)->create();
 
-        $response = $this->actingAs($this->getUserForIndex())->json('GET', route('api.files.index'));
+		$response = $this->actingAs($this->getUserForIndex())->json('GET', route('api.files.index'));
 
-        $response->assertStatus(200);
-        $response->assertJson([
-            "status" => "success",
-            "payload" => [
-                'files' => [
-                    'data' => $files->sortBy('file_name')->values()->toArray()
-                ]
-            ]
-        ]);
-    }
+		$response->assertStatus(200);
+		$response->assertJson([
+			"status" => "success",
+			"payload" => [
+				'files' => [
+					'data' => $files->sortBy('file_name')->values()->toArray()
+				]
+			]
+		]);
+	}
 
-    /** @test */
-    public function it_checks_if_index_gives_error()
-    {
-        $files = factory(File::class, 4)->create();
+	/** @test */
+	public function it_checks_if_index_gives_error()
+	{
+		$files = factory(File::class, 4)->create();
 
-        $response = $this->actingAs($this->getUserForIndexError())->json('GET', route('api.files.index'));
+		$response = $this->actingAs($this->getUserForIndexError())->json('GET', route('api.files.index'));
 
-        $response->assertStatus(403);
-        $response->assertJson([
-            'status' => "error",
-            'payload' => [
-                'errors' => []
-            ]
-        ]);
-    }
+		$response->assertStatus(403);
+		$response->assertJson([
+			'status' => "error",
+			'payload' => [
+				'errors' => []
+			]
+		]);
+	}
 
-    /** @test */
-    public function it_checks_create_works()
-    {
-        $response = $this->actingAs($this->getUserForCreate())->json('POST', route('api.files.store', [
-            'file_name' => 'something cute.png',
-            'file_type' => 'image/png',
-            'file_data' => $this->get1x1RedPixelImage()
-        ]));
+	/** @test */
+	public function it_checks_create_works_with_default_filesystem()
+	{
+		$response = $this->actingAs($this->getUserForCreate())->json('POST', route('api.files.store', [
+			'file_name' => 'something cute.png',
+			'file_type' => 'image/png',
+			'file_data' => $this->get1x1RedPixelImage()
+		]));
 
-        $file = File::first();
+		$file = File::first();
 
-        $response->assertStatus(200);
-        $response->assertJson([
-            'status' => "success",
-            'payload' => [
-                'file' => $file->toArray()
-            ]
-        ]);
-    }
+		$response->assertStatus(200);
+		$response->assertJson([
+			'status' => "success",
+			'payload' => [
+				'file' => $file->toArray()
+			]
+		]);
+	}
 
-    /** @test */
-    public function it_checks_update_works()
-    {
-        $file = factory(File::class)->create();
+	/** @test */
+	public function it_checks_create_works_with_s3_filesystem()
+	{
+		//force to s3 for this test
+		config(['filesystems.default' => 's3']);
 
-        $response = $this->actingAs($this->getUserForUpdate())->json('PUT', route('api.files.update', [
-            'file' => $file->id,
-            'file_name' => 'something cute.png',
-            'file_type' => 'image/png',
-            'file_data' => $this->get1x1RedPixelImage()
-        ]));
+		$file = factory(File::class)->make();
 
-        $file->refresh();
+		$response = $this->actingAs($this->getUserForCreate())->json('POST', route('api.files.store', [
+			'file_name' => $file['file_name'],
+			'file_type' => $file['file_type'],
+		]));
 
-        $response->assertStatus(200);
-        $response->assertJson([
-            'status' => "success",
-            'payload' => [
-                'file' => $file->toArray()
-            ]
-        ]);
-    }
+		$response->assertStatus(200);
+		$response->assertJson([
+			'status' => "success",
+			'payload' => [
+				'file' => [
+					'file_name' => $file['file_name'],
+					'file_type' => $file['file_type']
+				],
+				// Not checking "upload_url" since it is dynamic nonsense hash
+			]
+		]);
+	}
 
-    /** @test */
-    public function it_checks_delete_works()
-    {
-        $file = factory(File::class)->create();
+	/** @test */
+	public function it_checks_update_works()
+	{
+		$file = factory(File::class)->create();
 
-        $response = $this->actingAs($this->getUserForDelete())->json('DELETE', route('api.files.destroy', [
-            'file' => $file->id
-        ]));
+		$response = $this->actingAs($this->getUserForUpdate())->json('PUT', route('api.files.update', [
+			'file' => $file->id,
+			'file_name' => 'something cute.png',
+			'file_type' => 'image/png',
+			'file_data' => $this->get1x1RedPixelImage()
+		]));
 
-        $response->assertStatus(200);
-        $response->assertJson([
-            'status' => "success",
-            'payload' => []
-        ]);
-    }
+		$file->refresh();
+
+		$response->assertStatus(200);
+		$response->assertJson([
+			'status' => "success",
+			'payload' => [
+				'file' => $file->toArray()
+			]
+		]);
+	}
+
+	/** @test */
+	public function it_checks_delete_works()
+	{
+		$file = factory(File::class)->create();
+
+		$response = $this->actingAs($this->getUserForDelete())->json('DELETE', route('api.files.destroy', [
+			'file' => $file->id
+		]));
+
+		$response->assertStatus(200);
+		$response->assertJson([
+			'status' => "success",
+			'payload' => []
+		]);
+	}
 }
